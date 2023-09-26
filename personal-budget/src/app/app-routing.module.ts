154e31f44bd4f5a407c43387362ed6b22056a720
Jasti Lokesh <jastilokeshchowdary@gmail.com>
2023-09-25 20:47:21 -0400

    Assignment 5 Lokesh Jasti

--- personal-budget/src/app/app-routing.module.ts
+++ personal-budget/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { P404Component } from './p404/p404.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {
@@ -12,12 +13,16 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: "about",
+    path: 'about',
     component: AboutComponent
   },
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
   },
   {
     path: '**',
